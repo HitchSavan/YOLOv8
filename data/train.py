025bf9973babe28b6c1cf51e6514cf59d2ec119e
HitchSavan <temnyy1@gmail.com>
2023-04-06 19:56:38 +0300

    Trained NN

--- data/train.py
+++ data/train.py
@@ -1,9 +1,11 @@
 from ultralytics import YOLO
 import os
-path = os.getcwd()
 
-# Load a YOLOv8 model from a pre-trained weights file
-model = YOLO('yolov8m.pt')
+if __name__ == "__main__":
+    path = os.getcwd()
 
-# Run MODE mode using the custom arguments ARGS (guess TASK)
-model.train(data=f'{path}/ultralytics/data/hand.yaml', epochs=100, imgsz=640)
+    # Load a YOLOv8 model from a pre-trained weights file
+    model = YOLO('yolov8m.pt')
+
+    # Run MODE mode using the custom arguments ARGS (guess TASK)
+    model.train(data=f'{path}/data/hand.yaml', epochs=100, imgsz=640, batch=12)
