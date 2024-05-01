5cc16edbe512cc87b783d196557ef2b1c6bc1c6d
Daniil Temnyj <temnyy1@gmail.com>
2024-05-01 18:57:23 +0300

    Redirected train output to file

--- gesture_recognizer/gesture_train.py
+++ gesture_recognizer/gesture_train.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import tensorflow as tf
 assert tf.__version__.startswith('2')
 
@@ -18,11 +19,18 @@ data = gesture_recognizer.Dataset.from_folder(
     hparams=gesture_recognizer.HandDataPreprocessingParams()
 )
 
+
 train_data, rest_data = data.split(0.8)
 validation_data, test_data = rest_data.split(0.5)
 epochs=500
+model_name = f"model_clean_cropped_mediapipe_{epochs}epochs"
+
+orig_stdout = sys.stdout
+f = open(f'{model_name}.txt', 'w')
+sys.stdout = f
+
 delegate=mp.tasks.BaseOptions.Delegate.GPU
-hparams = gesture_recognizer.HParams(export_dir=os.path.join("gesture_recognizer", f"model_clean_cropped_mediapipe_{epochs}epochs"), epochs=epochs)
+hparams = gesture_recognizer.HParams(export_dir=os.path.join("gesture_recognizer", model_name), epochs=epochs)
 options = gesture_recognizer.GestureRecognizerOptions(hparams=hparams)
 model = gesture_recognizer.GestureRecognizer.create(
     train_data=train_data,
@@ -34,3 +42,6 @@ loss, acc = model.evaluate(test_data, batch_size=1)
 print(f"Test loss:{loss}, Test accuracy:{acc}")
 
 model.export_model()
+
+sys.stdout = orig_stdout
+f.close()
